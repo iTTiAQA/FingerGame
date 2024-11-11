9227b101c47d7d75815e35fad98395eecee8f7d1
iTTiAQA <97337795+iTTiAQA@users.noreply.github.com>
2024-11-11 22:56:45 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
 from player import Player
-
-
-def count_five(num1, num2):
-    """Used for counting shield in fun: detect_shield()"""
-    if num1 == 5 and num2 == 5:
-        return 2
-    elif num1 == 5 or num2 == 5:
-        return 1
-    else:
-        return 0
+from source import detect_shield
+from source import finger_add
 
 
 class FingerGame:
@@ -35,46 +27,9 @@ class FingerGame:
             if _input in choice:
                 return _input
 
-    def detect_shield(self, or_left, or_right, player):
-        """Used for counting shield after a step"""
-        gen_shield = count_five(player.left, player.right) - count_five(or_left, or_right)
-        if gen_shield > 0:
-            player.shield += 5*gen_shield
-        else:
-            cur_shield = 5*count_five(player.left, player.right)
-            if player.shield > cur_shield:
-                player.shield = cur_shield
-            else:
-                pass
-
     def is_dead(self):
         if self.player1.HP <= 0 or self.player2.HP <= 0:
             return True
-
-    def _add(self, player1, hand1, player2=None, hand2=None):
-        # if add to another player
-        if player2:
-            if hand1 == "left":
-                if hand2 == "left":
-                    player1.left += player2.left
-                else:
-                    player1.left += player2.right
-            # if hand1 == "right":
-            else:
-                if hand2 == "left":
-                    player1.right += player2.left
-                else:
-                    player1.right += player2.right
-
-        # if add to self
-        else:
-            if hand1 == "left":
-                player1.left += player1.right
-            else:
-                player1.right += player1.left
-
-        player1.left %= 10
-        player1.right %= 10
 
     def a_step_player1(self):
         or_left = self.player1.left
@@ -91,20 +46,20 @@ class FingerGame:
             if_self = self.super_input(["yes", "no"], "If add to yourself:")
 
             if if_self == "yes":
-                self._add(self.player1, hand)
+                finger_add(self.player1, hand)
             else:
                 # Choose which hand to add
                 hand2 = self.super_input(["left", "right"], f"Add {hand} with player2's:")
-                self._add(self.player1, hand, self.player2, hand2)
+                finger_add(self.player1, hand, self.player2, hand2)
 
             # detect shield
-            self.detect_shield(or_left, or_right, self.player1)
+            detect_shield(or_left, or_right, self.player1)
             return "normal"
 
         # if action == "act":
         else:
             result = self.player1.use_weapon(self.player2, hand)
-            self.detect_shield(or_left, or_right, self.player1)
+            detect_shield(or_left, or_right, self.player1)
             return result
 
     def a_step_player2(self):
@@ -122,20 +77,20 @@ class FingerGame:
             if_self = self.super_input(["yes", "no"], "If add to yourself:")
 
             if if_self == "yes":
-                self._add(self.player2, hand)
+                finger_add(self.player2, hand)
             else:
                 # Choose which hand to add
                 hand2 = self.super_input(["left", "right"], f"Add {hand} with player1's:")
-                self._add(self.player2, hand, self.player1, hand2)
+                finger_add(self.player2, hand, self.player1, hand2)
 
             # detect shield
-            self.detect_shield(or_left, or_right, self.player2)
+            detect_shield(or_left, or_right, self.player2)
             return "normal"
 
         # if action == "act":
         else:
             result = self.player2.use_weapon(self.player1, hand)
-            self.detect_shield(or_left, or_right, self.player2)
+            detect_shield(or_left, or_right, self.player2)
             return result
 
     def run_game(self):
---
+++ source.py
@@ -0,0 +1,47 @@
+def count_five(num1, num2):
+    """Used for counting shield in fun: detect_shield()"""
+    if num1 == 5 and num2 == 5:
+        return 2
+    elif num1 == 5 or num2 == 5:
+        return 1
+    else:
+        return 0
+
+
+def detect_shield(or_left, or_right, player):
+    """Used for counting shield after a step"""
+    gen_shield = count_five(player.left, player.right) - count_five(or_left, or_right)
+    if gen_shield > 0:
+        player.shield += 5 * gen_shield
+    else:
+        cur_shield = 5 * count_five(player.left, player.right)
+        if player.shield > cur_shield:
+            player.shield = cur_shield
+
+
+def finger_add(player1, hand1, player2=None, hand2=None):
+    """Used for add fingers"""
+    # if add to another player
+    if player2:
+        if hand1 == "left":
+            if hand2 == "left":
+                player1.left += player2.left
+            else:
+                player1.left += player2.right
+        # if hand1 == "right":
+        else:
+            if hand2 == "left":
+                player1.right += player2.left
+            else:
+                player1.right += player2.right
+
+    # if add to self
+    else:
+        if hand1 == "left":
+            player1.left += player1.right
+        else:
+            player1.right += player1.left
+
+    # Make a reasonable range
+    player1.left %= 10
+    player1.right %= 10
